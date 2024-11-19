cd234f46932c777c347dcfe471a0fc5c728f8900
MIM <mim-oza86@mail.ru>
2024-11-19 07:35:14 +0300

    add Sinchronized

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,76 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static final Map<Integer, Integer> sizeToFreq = new ConcurrentHashMap<>();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws InterruptedException {
+        int numberOfThreads = 1000;
+        Thread[] threads = new Thread[numberOfThreads];
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            threads[i] = new Thread(() -> {
+                String route = generateRoute("RLRFR", 100);
+                int countR = countOccurrences(route, 'R');
+
+                // Обновление частот в потокобезопасной мапе
+                synchronized (sizeToFreq) { // Синхронизация доступа к мапе
+                    sizeToFreq.put(countR, sizeToFreq.getOrDefault(countR, 0) + 1);
+                }
+            });
+            threads[i].start();
         }
+
+        // Ждем завершения всех потоков
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        // Вывод результатов
+        printResults();
+    }
+
+    public static String generateRoute(String letters, int length) {
+        Random random = new Random();
+        StringBuilder route = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            route.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return route.toString();
+    }
+
+    public static int countOccurrences(String route, char c) {
+        int count = 0;
+        for (char ch : route.toCharArray()) {
+            if (ch == c) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void printResults() {
+        // Поиск самого частого количества повторений
+        int maxFrequency = 0;
+        int mostFrequentCount = 0;
+
+        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
+            if (entry.getValue() > maxFrequency) {
+                maxFrequency = entry.getValue();
+                mostFrequentCount = entry.getKey();
+            }
+        }
+
+        System.out.println("Самое частое количество повторений " + mostFrequentCount + " (встретилось " + maxFrequency + " раз)");
+        System.out.println("Другие размеры:");
+        int finalMostFrequentCount = mostFrequentCount;
+        sizeToFreq.forEach((size, freq) -> {
+            if (size != finalMostFrequentCount) {
+                System.out.println("- " + size + " (" + freq + " раз)");
+            }
+        });
     }
 }
